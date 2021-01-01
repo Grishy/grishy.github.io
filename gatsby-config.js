82df625379f3cf04ba221ca590357d3e8ff1748c
Grishy <mailgrishy@gmail.com>
2021-01-01 21:53:35 +0300

    feat(favicon): use svg icon for souce

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/favicon.png`,
+        icon: `content/assets/favicon.svg`,
       },
     },
     {
